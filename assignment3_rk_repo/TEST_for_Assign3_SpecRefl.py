30c8fbb1265a548a7f8101e56bd4c73787228df8
Robin Kwik <68877174+robinkwik@users.noreply.github.com>
2020-09-16 20:52:07 -0400

    Update TEST_for_Assign3_SpecRefl.py
    Added another function

--- assignment3_rk_repo/TEST_for_Assign3_SpecRefl.py
+++ assignment3_rk_repo/TEST_for_Assign3_SpecRefl.py
@@ -4,6 +4,11 @@ Created on Wed Sep 16 18:54:04 2020
 
 @author: robin
 """
-from Assign3_SpecRefl import ndviSpectralReflectance
+from Assign3_SpecRefl import ASDSpectralReflectance
 
-ndviSpectralReflectance('Paul_20200527.csv')
+ASDSpectralReflectance('Paul_20200527.csv')
+
+
+from Assign3_SpecRefl import specReflPlot
+
+specReflPlot('spectralReflectance.xlsx')
